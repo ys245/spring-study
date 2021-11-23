a6a0d5b7f0ddb051e89d38086699ae2a035e314f
ys245 <2450520714@qq.com>
2021-11-23 12:45:20 +0800

    尝试提交修改内容

--- spring-01/src/main/java/com/ys/bao1/Student.java
+++ spring-01/src/main/java/com/ys/bao1/Student.java
@@ -3,6 +3,19 @@ package com.ys.bao1;
 public class Student {
     private String name;
     private int age;
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
